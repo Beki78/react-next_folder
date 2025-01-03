82e0daa2fc4a1b98486ee29e7b121129765b18f0
Bereket Getachew <bakiget78@gmail.com>
2025-01-04 02:08:39 +0300

    reduce size from 15kb to 12.5kb by adding question.ts

--- src/script/create_components_folder.ts
+++ src/script/create_components_folder.ts
@@ -1,34 +1,12 @@
 #!/usr/bin/env node
 
-import inquirer from "inquirer";
-import { createJsxFile, createTsxFile } from "../utils/jsx_tsx_folder";
-import { createTsFile } from "../utils/ts_folder";
 import { folderCompJs, folderCompTs, folderTs } from "../class/objects";
+import { createScript } from "../questions/question";
 
 
 
-const createFolderScript = async () => {
-  try {
-    const questions = await inquirer.prompt([
-      {
-        type: "confirm",
-        name: "isTypescript",
-        message: "Are you using Typescript?",
-        default: false,
-      },
-    ]);
-
-    const { isTypescript } = questions;
-
-    if (!isTypescript) {
-      await createJsxFile(folderCompJs);
-    } else if(isTypescript){
-      await createTsxFile(folderCompTs);
-      await createTsFile(folderTs);
-    }
-  } catch (error) {
-    console.error("Error during prompt or file creation:", error);
-  }
+const createFolderScript =  () => {
+  createScript(folderCompJs, folderCompTs, folderTs);
 };
 
 createFolderScript();
